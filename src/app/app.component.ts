47651d1bea64cd0e45feafc6a442462f2600ff71
Sergio <sergio.mendozazezatti@gmail.com>
2019-11-17 10:58:03 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mat-msgp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mat-mulit-select-grid-project';
+}
