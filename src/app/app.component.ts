5f7e67303b713ab0bcb5cf79f3e817eafa1b92af
Sergio <sergio.mendozazezatti@gmail.com>
2019-11-20 21:47:02 -0600

    add bind properties examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,12 @@ export class AppComponent {
 
   formGroup: FormGroup;
   selectedNumbers: number[];
-  showClearButton = true;
   usedPipe: PipeTransform;
 
   numberList$: BehaviorSubject<any[]>;
   weeksList$: BehaviorSubject<any[]>;
   heroesList$: BehaviorSubject<any[]>;
+  developmentList$: BehaviorSubject<any[]>;
 
   constructor(private formBuilder: FormBuilder) {
     const numberList = Array.from(Array(50), (_, x) => x + 1);
@@ -30,18 +30,27 @@ export class AppComponent {
     const heroesList = ['Spiderman', 'Ironman', 'Thor', 'Hulk',
     'Captain America', 'Black Widow', 'Hawkeye', 'Dr.Strange', 'Starlord', 'Rocket Racoon', 'Gamora', 'Groot', 'Drax'];
 
-    // const itemsList = [
-    //   {id: 1, nombre: 'Spiderman', otherLanguages: {englishName: 'Spiderman', spanishName: 'Hombre araña'}},
-    //   {id: 2, nombre: 'Ironman', otherLanguages: {englishName: 'Ironman', spanishName: 'Hombre de acero'}},
-    // ];
+    const developmentList = [
+      {id: 1, developer: {name: 'Sammuel', bestProject: {name: 'ProjectOne'}}},
+      {id: 2, developer: {name: 'Catherine', bestProject: {name: 'Social For All'}}},
+      {id: 3, developer: {name: 'Keanu', bestProject: {name: 'EnviroSafety'}}},
+      {id: 4, developer: {name: 'Angelina', bestProject: {name: 'MultiSelectGrid'}}},
+    ];
 
     this.numberList$ = new BehaviorSubject(numberList);
     this.weeksList$ = new BehaviorSubject(weeksList);
     this.heroesList$ = new BehaviorSubject(heroesList);
+    this.developmentList$ = new BehaviorSubject(developmentList);
 
     this.formGroup = this.createFormGroup();
 
     this.usedPipe = new WeekDayPipe();
+
+
+    this.formGroup.valueChanges.subscribe(data => {
+      console.log('valueChanges');
+      console.dir(data);
+    });
   }
 
   createFormGroup() {
@@ -54,18 +63,26 @@ export class AppComponent {
 
 
   onMonthDayAdded(value) {
-    console.log('onMonthDayAdded');
+    // console.log('onMonthDayAdded');
     // console.dir(value);
+    // console.log('forms');
+    // console.dir(this.selectedNumbers);
+    // console.dir(this.formGroup);
 
   }
 
   onMonthDayRemoved(value) {
-    console.log('onMonthDayRemoved');
+    // console.log('onMonthDayRemoved');
     // console.dir(value);
+    // console.log('forms');
+    // console.dir(this.selectedNumbers);
+    // console.dir(this.formGroup);
 
   }
 
   onMonthDayClear() {
-    console.log('onMonthDayClear');
+    // console.log('onMonthDayClear');
+    // console.dir(this.selectedNumbers);
+    // console.dir(this.formGroup);
   }
 }
